4b1d37242ac8e521a981782a4b4200100451dd24
jzwang <jzwang@gaojihealth.com>
2019-08-30 11:21:52 +0800

    add swagger assemble all Micro Service

---
+++ src/main/java/com/gold/wang/swagger/DocumentationConfig.java
@@ -0,0 +1,46 @@
+package com.gold.wang.swagger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.zuul.filters.Route;
+import org.springframework.cloud.netflix.zuul.filters.RouteLocator;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import springfox.documentation.swagger.web.SwaggerResource;
+import springfox.documentation.swagger.web.SwaggerResourcesProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@Primary
+public class DocumentationConfig implements SwaggerResourcesProvider {
+
+
+    @Autowired
+    private final RouteLocator routeLocator;
+
+    public DocumentationConfig(RouteLocator routeLocator) {
+        this.routeLocator = routeLocator;
+    }
+
+    @Override
+    public List<SwaggerResource> get() {
+        List<SwaggerResource> resources = new ArrayList<>();
+
+        resources.add(swaggerResource("default", "/v2/api-docs"));
+
+        List<Route> routes = routeLocator.getRoutes();
+        for (Route route : routes) {
+            resources.add(swaggerResource(route.getId(), route.getFullPath().replace("**", "v2/api-docs")));
+        }
+        return resources;
+    }
+
+    private SwaggerResource swaggerResource(String name, String location) {
+        SwaggerResource swaggerResource = new SwaggerResource();
+        swaggerResource.setName(name);
+        swaggerResource.setLocation(location);
+        swaggerResource.setSwaggerVersion("2.0");
+        return swaggerResource;
+    }
+}
